f134d5c11506297122539a8bf0c04e43d0379946
Md Golam Jakaria <58362149+MdGolamJakaria@users.noreply.github.com>
2020-03-08 23:02:19 +0600

    done

--- js/main.js
+++ js/main.js
@@ -1,39 +1,51 @@
 // Start login button event handler
 const logInBtn = document.getElementById("login");
-logInBtn.addEventListener("click", function(){
+ logInBtn.addEventListener("click", function(){
     const logInArea = document.getElementById("log-Area");
     logInArea.style.display="none";
     const accountArea = document.getElementById("Account-Area");
     accountArea.style.display="block";
 })
 // End login button event handler
+
+
 // Start deposit button event handler
     const addDeposit = document.getElementById("deposit-Btn");
     addDeposit.addEventListener("click",function(){
-        const depositAmount = document.getElementById("deposit-amount").value;
-        const depositNumber = parseFloat(depositAmount);
+        const depositNumber = getInputNumber("deposit-amount")
 
         UpdateSpanText("current-deposit", depositNumber );
         UpdateSpanText("current-balance", depositNumber );
-        document.getElementById("deposit-amount").value = "";
 
-        
+        emptyText = emptyInputBox("deposit-amount");
 })
 
 // Start withdraw button event handler
     const withdrawBtn = document.getElementById("withdraw-Btn");
         withdrawBtn.addEventListener("click",function(){
-        const withdrawAmount = document.getElementById("withdraw-amount").value;
-        const withdrawNumber = parseFloat(withdrawAmount);
-        
-        const currentWithdraw = document.getElementById("current-withdraw").innerText;
+        const withdrawNumber = getInputNumber("withdraw-amount");
 
+        UpdateSpanText("current-withdraw", withdrawNumber) 
+        UpdateSpanText("current-balance", -1 * withdrawNumber);
 
+        emptyText = emptyInputBox("withdraw-amount");
     })
-    
 
 
+// input box cleaning function
+    function emptyInputBox(id){
+        document.getElementById(id).value = "";
+    }
 
+// get input Number Function
+    function getInputNumber(id){
+        const amount = document.getElementById(id).value;
+        const Number = parseFloat(amount);
+        return Number;
+    }
+
+
+// get span number function 
 
 function UpdateSpanText(id, depositNumber){
         const Current = document.getElementById(id).innerText;
